01049bd4eb21108617199c3321db801b41b0b3b5
Александр <0999353523ak@gmail.com>
2024-06-12 14:32:50 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,34 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    // WRITE YOUR CODE HERE
+    const list = document.createElement("ul");
+    for(const key in data){
+        if (data.hasOwnProperty(key)) {
+            const elementOfList = document.createElement("li");
+            elementOfList.textContent = key;
+            if (Object.keys(data[key]).length > 0) createTree(elementOfList, data[key]);
+            list.appendChild(elementOfList);
+        }
+        element.appendChild(list);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
